af42fc3930b86c4abf578920d476851dbc77bebf
bryan vera <bveracachay@gmail.com>
2022-06-24 17:21:40 -0500

    se agreg parte del hombre

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,jsx,ts,js}'],
+  content: [
+    './index.html',
+    './src/**/*.{vue,jsx,ts,js}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {
       colors: {
         'botton-blue': '#010066',
+        sideblue: '#002c81',
         'top-blue': '#0c1540',
         'white-fondo': '#F1F1F1',
         'grey-ancla': '#7B7B7B',
@@ -18,5 +23,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 };
