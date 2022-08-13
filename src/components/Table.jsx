f5ba218efe00ddf77f46c94906f06bd082ee01a1
bryan vera <bveracachay@gmail.com>
2022-08-13 12:43:12 -0500

    se soluciona probelma del refresh de data agregada

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -49,14 +49,14 @@ const Table = ({ data, error, loading }) => {
     let page = current_page || pageFinal,
       per_page = per_page_items || 10,
       offset = (page - 1) * per_page,
-      paginatedItems = items.slice(offset).slice(0, per_page_items),
-      total_pages = Math.ceil(items.length / per_page);
+      paginatedItems = items?.slice(offset).slice(0, per_page_items),
+      total_pages = Math.ceil(items?.length / per_page);
     setPageInitial({
       page: page,
       per_page: per_page,
       pre_page: page - 1 ? page - 1 : null,
       next_page: total_pages > page ? page + 1 : null,
-      total: items.length,
+      total: items?.length,
       total_pages: total_pages,
       data: paginatedItems,
     });
